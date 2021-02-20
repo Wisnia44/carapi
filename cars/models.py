7a55505d4668c46f1a448784bd7d15c5fa7a6a67
Tomek Wiśniewski <tttttttttttttt7@gmail.com>
2021-02-20 14:27:39 +0100

    Hosting the app on heroku

--- cars/models.py
+++ cars/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Car(models.Model):
 	make = models.CharField(max_length=50)
 	model = models.CharField(max_length=50)
-	avg_rating = models.FloatField(blank=True, null=True)
+	avg_rating = models.FloatField(default=0)
 	rates_number = models.PositiveSmallIntegerField(default=0)
 	# I assume that GET Car/Cars would be called more frequently that POST Rate
 	# so the solution where avg_ratings and rates_number is part of Car model is optimal
